76eb82704c3589affc6f593fbf0a953ad8cae5c6
João Neto <j.altevirneto@gmail.com>
2024-10-29 10:27:14 -0300

    feat: add expedition model verification

---
+++ src/models/expeditionModel.js
@@ -0,0 +1,26 @@
+import {Schema, model} from mongoose
+
+const expeditionSchema = new Schema({
+    location: {
+        type: Schema.Types.String,
+        required: true
+    },
+    date: {
+        type: Schema.Types.Date,
+        required: true
+    },
+    partipants: {
+        type:[Schema.Types.ObjectId],
+        required: true,
+        ref: "Explorer"
+    },
+    speciesFound: {
+        type: [Schema.Types.ObjectId],
+        required: false,
+        ref: "Species"
+    }
+})
+
+const Expedition = model("Expedition", expeditionSchema);
+
+export default Expedition

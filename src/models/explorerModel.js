456c44382151140f4ed563f91d304463e16d0eee
João Neto <j.altevirneto@gmail.com>
2024-10-29 09:15:04 -0300

    feat: add verification model for explorer

---
+++ src/models/explorerModel.js
@@ -0,0 +1,21 @@
+import {Schema, model} from mongoose;
+
+const explorerSchema = new Schema({
+    name: {
+        type: Schema.Types.String,
+        required: true
+    },
+    fieldOfStudy: {
+        type: Schema.Types.String, 
+        required: true
+    },
+    expeditionsParticipated: {
+        type: Schema.Types.Number,
+        required: false,
+        default: 0
+    }
+})
+
+const Explorer = model("Explorer", explorerSchema);
+
+export default Explorer
